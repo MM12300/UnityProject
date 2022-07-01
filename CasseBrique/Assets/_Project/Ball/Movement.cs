183dcf973202f51a98d69533429686189125b2a8
cyrielletouton <cyrielle.t@hotmail.fr>
2022-07-01 11:34:04 +0200

    random direction ball

--- CasseBrique/Assets/_Project/Ball/Movement.cs
+++ CasseBrique/Assets/_Project/Ball/Movement.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float speed;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 randomVector = new Vector3(Random.Range(-1f, 1f), 0, 1);
+
         if (Input.GetKey(KeyCode.Space))
         {
-            this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * speed);
+            this.gameObject.GetComponent<Rigidbody>().AddForce(randomVector * speed);
         }
     }
 }
